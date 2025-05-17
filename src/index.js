1fa74aac5729819cd8f779c43b23909a558760a5
unknown <ozavciosman18@gmail.com>
2025-05-17 17:27:27 +0300

    Route'lar eklendi, zamanlanmış görevler hala devre dışı

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-// Routes - Şimdilik devre dışı bırakıyoruz
-// const prayerTimeRoutes = require('./routes/prayerTimes');
-// const countryRoutes = require('./routes/countries');
-// const stateRoutes = require('./routes/states');
-// const cityRoutes = require('./routes/cities');
+// Routes
+const prayerTimeRoutes = require('./routes/prayerTimes');
+const countryRoutes = require('./routes/countries');
+const stateRoutes = require('./routes/states');
+const cityRoutes = require('./routes/cities');
 
 // Scripts - Şimdilik devre dışı bırakıyoruz
 // const { scheduleMonthlyCleanup } = require('./scripts/cleanupOldPrayerTimes');
@@ -32,18 +32,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route Middlewares - Şimdilik devre dışı bırakıyoruz
-// app.use('/api/prayer-times', prayerTimeRoutes);
-// app.use('/api/countries', countryRoutes);
-// app.use('/api/states', stateRoutes);
-// app.use('/api/cities', cityRoutes);
+// Route Middlewares
+app.use('/api/prayer-times', prayerTimeRoutes);
+app.use('/api/countries', countryRoutes);
+app.use('/api/states', stateRoutes);
+app.use('/api/cities', cityRoutes);
 
 // Sağlık kontrolü
 app.get('/', (req, res) => {
   console.log('Ana sayfa isteği alındı');
   res.json({ 
     status: 'success', 
-    message: 'Namaz Vakti API çalışıyor - Veritabanı Bağlantısı Eklendi',
+    message: 'Namaz Vakti API çalışıyor - Route\'lar Eklendi',
     env: process.env.NODE_ENV || 'development',
     time: new Date().toISOString()
   });
